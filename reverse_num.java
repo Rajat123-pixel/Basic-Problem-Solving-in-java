8f688b6968d8a2890e9ce059d714247b433f323f
Rajat123-pixel <73467043+Rajat123-pixel@users.noreply.github.com>
2022-07-17 13:08:11 +0530

    Add files via upload

---
+++ reverse_num.java
@@ -0,0 +1,19 @@
+package recursion;
+
+public class reverse_num {
+
+	public static void main(String[] args) {
+		 
+		fun(5);
+
+	}
+	
+	static void fun(int n) {
+		
+		//base condition//
+		if (n==0)
+			return;
+	}
+	System.out.println(n);
+
+}
